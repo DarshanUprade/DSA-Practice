d84b8034e1cbbd19779cf64843f17602fcb22573
DarshanUprade <darshanuprade22@gmail.com>
2025-10-24 21:48:58 +0530

    Logic initiated

--- file1.java
+++ file1.java
@@ -1,6 +1,18 @@
 class file1{
+	private static Connection  con=null;
+	private DBConnection() {}
+	static {
+	 try {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+			con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","darshan","core");
+		}
+		catch(Exception e) {e.printStackTrace();}
+		}//end of static block
+		public static Connection getCon() {
+		return con;
+	}
 	public static void main(String args[]){
-		System.out.println("Database Connection is Added");
+		System.out.println("Database Connection is Added"+con);
 		System.out.println("Prepared Statement added");
 		System.out.println("Excecuting Query");
 		System.out.println("ResultSet Object Created");
